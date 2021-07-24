5958312aabee9bde5a6a74c016c9f265f7e6ace5
Maggie Ward <mlward639@gmail.com>
2021-07-24 00:58:06 -0400

    added columns to category model

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
   },
   {
     sequelize,
@@ -16,5 +26,16 @@ Category.init(
     modelName: 'category',
   }
 );
+
+
+/* ADD IF NEEDED FOR ASSOCIATIONS: 
+    traveller_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'traveller',
+        key: 'id',
+        unique: false
+      }
+    }, */
 
 module.exports = Category;
